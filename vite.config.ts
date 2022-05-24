97bc36b7df0332ded3eac439063bcc3f72e91683
zwq <2467776065@qq.com>
2022-05-24 22:58:03 +0800

    对项目开发和git commit时进行eslint规范

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import eslint from 'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), eslint()],
   server: {
-		port: 3004,
-		strictPort: true
-	}
+    port: 3004,
+    strictPort: true
+  }
 })
